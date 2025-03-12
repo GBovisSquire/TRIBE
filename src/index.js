8d883cc6a2d02a6312cdb60eb1af1a4dd041bad6
GBovisSquire <gracejbsquire@gmail.com>
2025-03-12 14:51:35 +0000

    css code organised and more changes to about page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+document.querySelectorAll(".toggle-header").forEach((header) => {
+  header.addEventListener("click", () => {
+    const toggle = header.parentElement;
+    toggle.classList.toggle("active");
+  });
+});
+
 new Typewriter(".typewriter", {
   strings: "Let's go on an adventure...",
   autoStart: true,
